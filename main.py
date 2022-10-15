11bbda30614f57769c3d4eddcd0857e661c12e38
Виталий Чухляев <vitaliy69@mail.ru>
2022-10-15 17:04:39 +0300

    I did 'lastname,firstname,surname,organization'

--- main.py
+++ main.py
@@ -5,35 +5,40 @@ from pprint import pprint
 # читаем адресную книгу в формате CSV в список contacts_list
 import csv
 
+contacts_list = []
+
+header_list = ['lastname', 'firstname', 'surname', 'organization']
+
+reg_ex4 = r'^([А-я]\w+),\s?([А-я]\w+),\s?([А-я]\w+|)|(ФНС|Минфин)|'
+
 with open("phonebook_raw.csv") as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
-# pprint(contacts_list)
-x = contacts_list[1]
-z = str(x)
-pprint(z)
-print(type(z))
-# reg
-reg_ex4 = r'^\W+(\b[а-яА-Я\w]+\b)\W*(\b[а-яА-Я\w]+\b)\W*(\b[а-яА-Я\w]+\b)*\''
-reg_posish = r"(?P<q>[\s])'(\b[а-яА-Я\w]+\b)\s?(\b[а-яА-Я\w]+\b)\s+.+?\s*(\b[а-яА-Я\w]+\b)\s*?(\b[а-яА-Я\w]+\b)\s*?'(" \
-             r"?P<k>[\S]) "
-reg_fone = r'^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$'
-reg_organ = r'ФНС|Минфин'
+    # rows = csv.DictReader(f, delimiter=" ")
+    rows = csv.reader(f, delimiter=" ")
 
 
-i = 1
-while i != len(contacts_list):
-    print(re.findall(,reg_ex4 str(contacts_list[i])))
-    print(re.findall(reg_organ, str(contacts_list[i])))
-    print(re.findall(reg_posish, str(contacts_list[i])))
-    print(re.findall(reg_fone, str(contacts_list[i])))
-    i += 1
+    for row in rows:
+        x = ','.join(row)
+        fio = re.findall(reg_ex4, x)
+        fio = re.findall(r'(\w+)', str(fio))
+        contacts_list.append(fio)
 
 
 
 
+    with open('new_phonebook.csv', 'w', encoding='utf-8') as f:
+        i = 1
+        writer = csv.writer(f, delimiter=",")
+        writer.writerow(header_list)
+        while i != len(contacts_list):
+            writer.writerow(contacts_list[i])
+            i += 1
 
 
+
+# reg_ex4 = r'^\S\'(\b[Аа-я\w]+\b)\'?\s?\S?\s\S?(\b[Аа-я\w]+\b)\s?\S*?\W*?(\b[А-я\w]+\b)'
+# reg_fone = r'(\+\d|\d)\s*(\(|)(\d{3})[\s\)-]*(\d{3})\-*(\d{2})\-*(\d{2})'
+# reg_fone1 = r'(\+\d|\d)\s*(\(|)(\d{3})[\s\)-]*(\d{3})\-*(\d{2})\-*(\d{2})\W*?.(доб.\s\d{4})'
+# reg_fone2 = r'[,](\+7|8),?((495)|\(495\))(-|,)?([0-9]{3})?-?([0-9]{2}?)-?([0-9]{2})'
 
 
 
